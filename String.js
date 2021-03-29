5886431d0ecd037fa8d110343bd1d4b1fb64b9d4
lina mierkE <lina_maria89@hotmail.com>
2021-03-29 10:37:30 -0400

    reverse option 2

--- String.js
+++ String.js
@@ -12,3 +12,16 @@ function reverseString(str) {
   
   reverseString("hello");
 
+//OPTION 2
+// create a for loop 
+function reverseString(str) {
+    for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+  }
+  /*Starting at the last character of the string passed to the function, you build a new string reversedStr from str.
+
+During each iteration of the for loop, reversedStr gets concatenated with itself and the current character.
+
+Finally, you return the final value of reversedStr. */
