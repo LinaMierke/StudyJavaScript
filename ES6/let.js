fc6aa1d951c145cbb906ddd160d12bcd960f06b9
lina mierkE <lina_maria89@hotmail.com>
2021-04-14 16:34:20 -0400

    variables es6

---
+++ ES6/let.js
@@ -0,0 +1,14 @@
+// use let because with var the variable can be reassign,
+//with let you are use string declaration because once the variable is assign you can't use it again
+
+
+var hi = "hi";
+var hi = "hola";
+
+//result will be hola because is the last one 
+
+
+let hi="hi";
+let hi ="hola"
+
+//result will be  hi is not defined.
