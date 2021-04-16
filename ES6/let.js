21aede5858caae1039a309482c9896442e51be78
lina mierkE <lina_maria89@hotmail.com>
2021-04-16 11:50:49 -0400

    var and let

--- ES6/let.js
+++ ES6/let.js
@@ -12,3 +12,15 @@ let hi="hi";
 let hi ="hola"
 
 //result will be  hi is not defined.
+/*
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+*/
