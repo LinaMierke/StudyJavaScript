1735b2bae7c2b76441b9cc4c227ab25cf34ff866
lina mierkE <lina_maria89@hotmail.com>
2021-04-23 17:06:50 -0400

    string

---
+++ ES6/string.js
@@ -0,0 +1,6 @@
+// the new `` and ${const} 
+//there you can run a function or any operation like this case: calculation of dog yearas and 7 
+
+const name = "Snicker"
+const age = 2;
+const sentence = `My dog ${name} is ${age * 7 } years old`
